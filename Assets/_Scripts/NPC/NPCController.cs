9173a76ac0267f31831ad44b723d1d6fa9671647
SandSide <37347875+SandSide@users.noreply.github.com>
2023-07-08 19:01:57 +0100

    fear: Implement npc controller
    - Implement npc controller script to mange npc
    
    - Implement npc walking in random direction
    
    - Implement change direction after a timer runs out

---
+++ Assets/_Scripts/NPC/NPCController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class NPCController : MonoBehaviour
+{
+    public static float speed = 1f;
+    public float maxChangeDirectionInterval = 8f;
+    public float currentChangeInterval;
+    public Vector2 targetDirection;
+    public bool canMove = false;
+    
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb  = GetComponent<Rigidbody2D>();
+
+        currentChangeInterval = Random.Range(2, maxChangeDirectionInterval + 1);
+        targetDirection = Random.onUnitSphere*10;
+        canMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canMove)
+        {
+            currentChangeInterval -= Time.deltaTime;
+           
+            if(currentChangeInterval < 0)
+            {
+                currentChangeInterval = Random.Range(2, maxChangeDirectionInterval + 1);
+                targetDirection = Random.onUnitSphere*10;
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, targetDirection, speed * Time.deltaTime);
+
+        }
+    }
+}
--- Assets/_Scripts/Quest/QuestController.cs
+++ Assets/_Scripts/Quest/QuestController.cs
@@ -25,7 +25,6 @@ public class QuestController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if(Input.GetKeyDown(KeyCode.Space))
             StartNewQuest();
 
